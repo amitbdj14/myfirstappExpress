e2872cf8f3910da31ce3f859ca5415955ac2464f
Amit Kumar Bhardwaj <amit.bhardwaj@emirates.com>
2018-02-22 13:27:45 +0400

    test

--- routes/things.js
+++ routes/things.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res){
-   res.send('GET route on things.');
-});
-router.post('/', function(req, res){
-   res.send('POST route on things.');
-});
+var app=express();
 
+
+app.get('/:id', function(req, res){
+    res.send('The id you specified is '+ req.params.id);
+ });
+ app.get('/things/:name/:id', function(req, res) {
+    res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
+ });
+
+ app.get('/things/:id([0-9]{5})', function(req, res){
+    res.send('id: ' + req.params.id);
+ });
+ app.listen(3060);
 //export this router to use in our index.js
 module.exports = router;
